40af62da947376da96de44b652150260b9a62d8a
Marcal <marcalsantarem@gmail.com>
2022-11-09 22:39:11 -0300

    Seller entity class

--- demo-dao-jdbc/src/application/Program.java
+++ demo-dao-jdbc/src/application/Program.java
@@ -1,13 +1,18 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 	
 	public static void main(String[] args) {
 		
 		Department obj = new Department(1, "Teste");
-		System.out.println(obj);
+		Seller seller = new Seller(21, "Marcal", "marcal@gmail.com", new Date(), 5000.00, obj);
+		
+		System.out.println(seller);
 		
 	}
 
